c969dac8cd51298504cbaf98a27f1a31635fdbfc
NickFlppv <nikolay.filippov.1999@mail.ru>
2020-02-24 10:10:07 +0300

    Added OAuth project

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
